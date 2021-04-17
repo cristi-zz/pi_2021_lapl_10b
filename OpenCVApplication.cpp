fa4eb01126333c346662c5e1f4ab9cb196cb7d08
Alexandra Opris <sandraopris@yahoo.com>
2021-04-17 13:12:33 +0300

    Laplace

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -64,26 +64,42 @@ void testColor2Gray()
 	}
 }
 
-void gaussianPyramid(Mat src) {
-	Mat dst1, dst2, dst3;
-
-	pyrDown(src, dst1, Size(src.rows/2, src.cols/2));
-	pyrDown(dst1, dst2, Size(src.rows/4, src.cols/4));
-	pyrDown(dst2, dst3, Size(src.rows/8, src.cols/8));
-
-	imshow("Original", src);
-	imshow("1", dst1);
-	imshow("2", dst2);
-	imshow("3", dst3);
-	waitKey();
+void gaussianPyramid(Mat src, Mat *dst1, Mat* dst2, Mat* dst3) {
+	pyrDown(src, *dst1, Size(src.rows/2, src.cols/2));
+	pyrDown(*dst1, *dst2, Size(src.rows/4, src.cols/4));
+	pyrDown(*dst2, *dst3, Size(src.rows/8, src.cols/8));
 }
 
-void printGaussian() {
-	Mat src;
+void laplacianPyramid(Mat src1, Mat src2, Mat src3, Mat* dst1, Mat* dst2) {
+	Mat l1, l2, l3;
+	pyrUp(src1, l1, Size(src1.rows*2, src1.cols*2));
+	pyrUp(src2, l2, Size(src2.rows*2, src2.cols*2));
+	pyrUp(src3, l3, Size(src3.rows*2, src3.cols*2));
+
+	subtract(src1, l2, *dst1);
+	subtract(src2, l3, *dst2);
+}
+
+void printLevels() {
+	Mat src, dst1, dst2, dst3;
 	char fname[MAX_PATH];
 	openFileDlg(fname);
 	src = imread(fname, IMREAD_GRAYSCALE);
-	gaussianPyramid(src);
+	gaussianPyramid(src, &dst1, &dst2, &dst3);
+
+	imshow("Original image", src);
+	imshow("Gaussian 1", dst1);
+	imshow("Gaussian 2", dst2);
+	imshow("Gaussian 3", dst3);
+	
+	Mat dst4, dst5, dst6;
+
+	laplacianPyramid(dst1, dst2, dst3, &dst4, &dst5);
+
+	imshow("Laplace 1", dst4);
+	imshow("Laplace 2", dst5);
+
+	waitKey();
 }
 
 
@@ -98,7 +114,7 @@ int main()
 		printf(" 1 - Deschidere imagine...!\n");
 		printf(" 2 - Open BMP images from folder\n");
 		printf(" 3 - Color to Gray\n");
-		printf(" 4 - Show Gaussian levels\n");
+		printf(" 4 - Show Gaussian & Laplacian levels\n");
 		printf(" 0 - Exit\n\n");
 
 		printf("Option: ");
@@ -115,7 +131,7 @@ int main()
 				testColor2Gray();
 				break;
 			case 4:
-				printGaussian();
+				printLevels();
 		}
 	}
 	while (op!=0);
